9c7496e981868601ffdfdc83991fd1e91e91a08e
Luam <luam13@gmail.com>
2014-08-23 17:25:44 -0400

    stuff to remove

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Luamyemane::Application.config.secret_key_base = '0589b43e628b1e9912bc8c126bbfa2f2977c8567aec13a277f5e58a38d0186d49844b1dcc09a7706d3b56080bd62a64138998739bba91973c49fe19ae5468ce4'
